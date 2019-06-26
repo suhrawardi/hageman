1a88aa50166f0dbacab3f1b6ecfa54e528502180
jarra <suhrawardi@gmail.com>
2019-06-26 21:31:03 +0200

    Green

--- src/ChannelPanel.hs
+++ src/ChannelPanel.hs
@@ -30,9 +30,9 @@ adjustMode = proc (mode, mo) ->
         returnA -< mo
 
 
-asNote :: Int -> Maybe Int -> Int -> Maybe [MidiMessage]
-asNote c Nothing d  = Nothing
-asNote c (Just n) d = Just [ANote c n 100 01]
+asNote :: Int -> Maybe Int -> Int -> Double -> Maybe [MidiMessage]
+asNote c Nothing  v d = Nothing
+asNote c (Just n) v d = Just [ANote c n 127 d]
 
 
 channelPanel :: UISF (Maybe OutputDeviceID, Double) (Maybe [MidiMessage])
@@ -41,13 +41,15 @@ channelPanel = topDown $ setSize (560, 670) $ title "Channel" $ proc (mo, f) -> 
     mode <- leftRight $ title "Mode" $ radio ["drone", "rhythm"] 0 -< ()
     d <- title "Dur" $ withDisplay (hiSlider 1 (1, 16) 1) -< ()
 
-    (note, tick) <- (| leftRight ( do
+    (note, vel, dur, tick) <- (| leftRight ( do
       tuning <- topDown $ setSize (270, 290) $ title "Scale" $ radio otherScales 0 -< ()
       tick' <- timer -< 1/f
       f' <- randPanel -< (f, tick')
-      tick <- timer -< (1/f') * fromIntegral d
+      let dur = (1/f') * fromIntegral d
+          vel = 127
+      tick <- timer -< dur
       note <- randNote -< (scale tuning, 3, tick)
-      returnA -< (note, tick) ) |)
+      returnA -< (note, vel, dur, tick) ) |)
 
     if isLearning
       then do
@@ -55,7 +57,7 @@ channelPanel = topDown $ setSize (560, 670) $ title "Channel" $ proc (mo, f) -> 
       else do
         if isPlaying
           then do
-            moM <- adjustMode -< (mode, asNote channel note 100)
+            moM <- adjustMode -< (mode, asNote channel note vel dur)
             returnA -< moM
           else
             returnA -< Nothing
--- src/Scales.hs
+++ src/Scales.hs
@@ -8,14 +8,14 @@ import HSoM
 
 
 scale :: Int -> [PitchClass]
-scale 1 = [C, Cs, D, Ds, E, F, Fs, G, Gs, A, As, B]
-scale 2 = [C, Cs,    Ds,       Fs, G,     A, As   ]
-scale 3 = [C,     D,     E,    Fs, G, Gs,    As   ]
-scale 4 = [C,     D,     E, F,     G, Gs,        B]
+scale 1 = [C, Cs,        E,    Fs, G,     A, As   ]
+scale 2 = [C,     D,     E,    Fs, G, Gs,    As   ]
+scale 3 = [C,     D,     E, F,     G, Gs,        B]
+scale 4 = [C,        Ds, E,    Fs, G,     A,     B]
 scale 5 = [C,     D,     E, F,     G,     A,     B]
 scale 6 = [C, Cs,        E, F, Fs,    Gs,        B]
 scale 7 = [C, Cs,    Ds,    F, Fs,    Gs,    As   ]
-scale 8 = [   Cs, D,     E, F, Fs,    Gs,    As   ]
+scale 8 = [C,     D,     E, F, Fs,    Gs,    As   ]
 scale 9 = [C, Cs,           F,     G, Gs          ]
 scale _ = [C, Cs, D, Ds, E, F, Fs, G, Gs, A, As, B]
 
